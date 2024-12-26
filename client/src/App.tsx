46a8c833f33442a3230a79dd0688607a2989fccf
Javad Ghofrani <19925866+ghofrani85@users.noreply.github.com>
2024-12-26 13:20:52 +0100

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import GitHubCorner from "./components/GitHubCorner"
 import Toast from "./components/toast/Toast"
 import EditorPage from "./pages/EditorPage"
 import HomePage from "./pages/HomePage"
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/editor/:roomId" element={<EditorPage />} />
+                    <Route path="/login" element={<LoginPage />} />
                 </Routes>
             </Router>
             <Toast /> {/* Toast component from react-hot-toast */}
